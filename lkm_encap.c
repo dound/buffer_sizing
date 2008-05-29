effee2a73a21697dd101a45bfd1d2cdec8028e05
dgu <dgu@2675338e-564e-0410-bbfd-a4d96e47a317>
2008-05-29 08:48:00 +0000

    added decap side
    git-svn-id: svn+ssh://pod.stanford.edu/afs/ir/users/d/g/dgu/svnrepos/mckeown/lkm_ipip_dgu@8 2675338e-564e-0410-bbfd-a4d96e47a317

--- lkm_encap.c
+++ lkm_encap.c
@@ -1,6 +1,7 @@
 /**
  * Filename: lkm_encap.c
- * Purpose: encapsulate outgoing packets in the special Bolouki IP-in-IP format
+ * Purpose:  encapsulate outgoing packets in the special Bolouki IP-in-IP format
+ * Author:   David Underhill (dgu@cs.stanford.edu) (May 2008)
  */
 
 #include <linux/ip.h>
@@ -11,17 +12,20 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/skbuff.h>
 
+#define _LKM_IPIP_DEBUG_ /* debug prints on and encap loopback traffic too */
+/* #define _LKM_IPIP_DO_DECAP_ */ /* decap IP-IP and Bolouki IP-IP packets */
+
 /** The number of bytes to put between the inner and outer IP headers. */
-#define PADDING_BW_IP_HEADERS 0
+#define PADDING_BW_IP_HEADERS 4
 
 /**
- * The IP protocol for the outer IP header.  It is 0xF4 for Bolouki encap, or
+ * The IP protocol for the outer IP header.  It is 0xF4 for IP-IP encap, or
  * 0x04 for IP-in-IP (e.g. PADDING_BW_IP_HEADERS == 0).
  */
 #if PADDING_BW_IP_HEADERS
-#define ENCAP_PROTO 0xF4 /* Bolouki IP-in-IP when padding separates headers */
+#define LKM_IPIP_PROTO 0xF4 /* Bolouki IP-in-IP when padding separates headers */
 #else
-#define ENCAP_PROTO 0x04 /* normal IP-in-IP when there is no padding */
+#define LKM_IPIP_PROTO 0x04 /* normal IP-in-IP when there is no padding */
 #endif
 
 /* IP addresses of destinations to do the encapsulation for */
@@ -34,9 +38,12 @@
 #define IP_ADDR_HBO_LOOPBACK   0x7F000001 /* 127.0.0.1 */
 
 /** who we want to address the encapsulation packet to (outer header) */
-#define IP_ADDR_HBO_DECAP_TARGET IP_ADDR_HBO_HOUSTON_1
+#define IP_ADDR_HBO_DECAP_TARGET IP_ADDR_HBO_LOOPBACK
 
-static struct nf_hook_ops netfilter_ops;
+static struct nf_hook_ops netfilter_encap;
+#ifdef _LKM_IPIP_DO_DECAP_
+static struct nf_hook_ops netfilter_decap;
+#endif
 
 /**
  * The implementation of this function is based on code in the following book:
@@ -68,6 +75,7 @@ static __u16 checksum( __u16* buf, unsigned len ) {
     return answer;
 }
 
+#ifdef _LKM_IPIP_DEBUG_
 /**
  * Prints an IP address in dotted-decimal format with printk.
  */
@@ -77,11 +85,28 @@ static void printk_ip( __u32 ip_nbo ) {
     bytes = (unsigned char*)&ip_nbo;
     printk( "%u.%u.%u.%u", bytes[0], bytes[1], bytes[2], bytes[3] );
 }
+#endif
+
+/**
+ * Modify the skb to tell other modules that we changed its contents.
+ */
+static void skb_flag_as_changed( struct sk_buff* skb ) {
+    /* Tell the netfilter framework that this packet is not the
+       same as the one before! */
+#ifdef CONFIG_NETFILTER
+    nf_conntrack_put( skb->nfct );
+    skb->nfct = NULL;
+#ifdef CONFIG_NETFILTER_DEBUG
+    skb->nf_debug = 0;
+#endif
+#endif
+}
 
 /**
  * Adjust headroom after the data is already in place.  This means the data from
  * skb->data (inclusive) to skb->tail (exclusive) will be shifted back towards
- * skb->end.
+ * skb->end.  skb->tail is updated to the new tail and skb->len is updated to
+ * the new len.
  *
  * @return If skb->tail + len > skb->end, then there is not enough space left
  * and the method will do nothing and return -1.  Otherwise, 0 will be returned.
@@ -102,17 +127,95 @@ static int skb_late_reserve( struct sk_buff* skb, unsigned len ) {
         *(--to) = *(--from);
 
     /* update the length of used bytes in the skb */
-    skb->len += len;
+    skb->tail += len;
+    skb->len  += len;
 
     /* success */
     return 0;
 }
 
+#ifdef _LKM_IPIP_DO_DECAP_
+/**
+ * Removes the first skip bytes of data.  Data from skb->data + skip to
+ * skb->tail is shifted up to fill in the hole.  skb->tail is updated to the new
+ * tail and skb->len is updated to the new len.
+ *
+ * @return -1 if skip is bigger than skb->tail - skb->data (e.g. it is bigger
+ * than the amount of data we have).  Otherwise, this will succeed and return 0.
+ */
+static int skb_remove( struct sk_buff* skb, unsigned skip ) {
+    unsigned i, real_data_len;
+    char* from, *to;
+
+    /* move the data bytes (from the last byte to the first byte) */
+    real_data_len = (unsigned)(skb->tail - skb->data);
+    if( real_data_len < skip )
+        return -1; /* can't skip more data than we have */
+
+    from = skb->data + skip;
+    to = skb->data;
+    for( i=0; i<real_data_len; i++ )
+        *(to++) = *(from++);
+
+    /* update the length of used bytes in the skb */
+    skb->tail -= skip;
+    skb->len  -= skip;
+
+    /* success */
+    return 0;
+}
+
+unsigned int decap_hook( unsigned int hooknum,
+                         struct sk_buff **pp_skb,
+                         const struct net_device *in,
+                         const struct net_device *out,
+                         int (*okfn)(struct sk_buff*) ) {
+    struct sk_buff* skb;
+    unsigned extra_len;
+
+    /* ignore packets with no data or IP header */
+    skb = *pp_skb;
+    if( !skb || !skb->nh.iph )
+        return NF_ACCEPT;
+
+    /* determine if we need to do decapsulation for this target */
+    if( skb->nh.iph->protocol == LKM_IPIP_PROTO ) {
+        extra_len = sizeof(struct iphdr) + PADDING_BW_IP_HEADERS;
+
+        /* make sure there are the expected # of bytes before we handle it */
+        if( unlikely(skb->len < extra_len) )
+            return NF_ACCEPT;
+
+#ifdef _LKM_IPIP_DEBUG_
+        printk( "LKM ENCAP: will decapsulate packet from " );
+        printk_ip( skb->nh.iph->saddr );
+        printk( " (proto=%u)\n", skb->nh.iph->protocol );
+#endif
+    }
+    else {
+#ifdef _LKM_IPIP_DEBUG_
+        printk( "LKM ENCAP: will not decapsulate packet from " );
+        printk_ip( skb->nh.iph->saddr );
+        printk( " (proto=%u)\n", skb->nh.iph->protocol );
+#endif
+        return NF_ACCEPT;
+    }
+
+    skb_flag_as_changed( skb );
+
+    /* overwrite the extra_len bytes at the front of skb->data */
+    skb_remove( skb, extra_len );
+
+    /* ok, give it back to the kernel */
+    return NF_ACCEPT;
+}
+#endif
+
 unsigned int encap_hook( unsigned int hooknum,
-                        struct sk_buff **pp_skb,
-                        const struct net_device *in,
-                        const struct net_device *out,
-                        int (*okfn)(struct sk_buff*) ) {
+                         struct sk_buff **pp_skb,
+                         const struct net_device *in,
+                         const struct net_device *out,
+                         int (*okfn)(struct sk_buff*) ) {
     struct sk_buff* skb;
     struct iphdr* outer_ip_hdr;
     struct iphdr* inner_ip_hdr;
@@ -131,30 +234,26 @@ unsigned int encap_hook( unsigned int hooknum,
     case IP_ADDR_HBO_LA_2:
     case IP_ADDR_HBO_NF_POWER_1:
     case IP_ADDR_HBO_NF_POWER_2:
+#ifdef _LKM_IPIP_DEBUG_
     case IP_ADDR_HBO_LOOPBACK:
         /* encapsulate the target ... I hope there's room in the SKB ... */
         printk( "LKM ENCAP: will encap this packet to " );
         printk_ip( skb->nh.iph->daddr );
         printk( "\n" );
+#endif
         break;
 
     default:
+#ifdef _LKM_IPIP_DEBUG_
         /* no ecapsulation needed */
         printk( "LKM ENCAP: will NOT encap this packet to " );
         printk_ip( skb->nh.iph->daddr );
         printk( "\n" );
+#endif
         return NF_ACCEPT;
     }
 
-    /* Tell the netfilter framework that this packet is not the
-       same as the one before! */
-#ifdef CONFIG_NETFILTER
-    nf_conntrack_put( skb->nfct );
-    skb->nfct = NULL;
-#ifdef CONFIG_NETFILTER_DEBUG
-    skb->nf_debug = 0;
-#endif
-#endif
+    skb_flag_as_changed( skb );
 
     /* Unfortunately, we cannot call skb_reserve once data is in place ... so we
      * have to manually push the data back to make room for the header.  We will
@@ -187,7 +286,7 @@ unsigned int encap_hook( unsigned int hooknum,
     outer_ip_hdr->frag_off = 0;
     outer_ip_hdr->id = inner_ip_hdr->id;
     outer_ip_hdr->ttl = inner_ip_hdr->ttl;
-    outer_ip_hdr->protocol = ENCAP_PROTO;
+    outer_ip_hdr->protocol = LKM_IPIP_PROTO;
     outer_ip_hdr->saddr = inner_ip_hdr->saddr;
     outer_ip_hdr->daddr = htonl( IP_ADDR_HBO_DECAP_TARGET );
 
@@ -200,32 +299,46 @@ unsigned int encap_hook( unsigned int hooknum,
                               sizeof(struct iphdr) + PADDING_BW_IP_HEADERS,
                               skb->csum);
 
-    /* ok, give the kernel back its back and hope it's ok with our changes */
+    /* ok, give it back to the kernel */
     return NF_ACCEPT;
 }
 
 int init_module() {
-    printk( "LKM ENCAP: Starting the Bolouki encapsulation LKM hook\n" );
+    printk( "LKM ENCAP: Starting the IP-IP encapsulation LKM hook\n" );
 
     /* tell netfilter where to give us a callback */
-    netfilter_ops.hook = encap_hook;
+    netfilter_encap.hook = encap_hook;
 
     /* handle IPv4 packets */
-    netfilter_ops.pf = PF_INET;
+    netfilter_encap.pf = PF_INET;
 
     /* call our hook before routing the packet */
-    netfilter_ops.hooknum = NF_IP_LOCAL_OUT;
+    netfilter_encap.hooknum = NF_IP_LOCAL_OUT;
 
     /* call our hook before any other hook */
-    netfilter_ops.priority = NF_IP_PRI_FIRST;
+    netfilter_encap.priority = NF_IP_PRI_FIRST;
 
     /* register our new callback hook for IPv4 packets */
-    nf_register_hook(&netfilter_ops);
+    nf_register_hook( &netfilter_encap );
+
+#ifdef _LKM_IPIP_DO_DECAP_
+    printk( "LKM ENCAP: Starting the IP-IP decapsulation LKM hook\n" );
+    netfilter_decap.hook = decap_hook;
+    netfilter_decap.pf = PF_INET;
+    netfilter_decap.hooknum = NF_IP_PRE_ROUTING;
+    netfilter_decap.priority = NF_IP_PRI_FIRST;
+    nf_register_hook( &netfilter_decap );
+#endif
 
     return 0;
 }
 
 void cleanup_module() {
-    nf_unregister_hook(&netfilter_ops);
-    printk( "LKM ENCAP: Terminating the Bolouki encapsulation LKM hook\n" );
+    nf_unregister_hook(&netfilter_encap);
+    printk( "LKM ENCAP: Terminated the IP-IP encapsulation LKM hook\n" );
+
+#ifdef _LKM_IPIP_DO_DECAP_
+    nf_unregister_hook(&netfilter_decap);
+    printk( "LKM ENCAP: Terminated the IP-IP decapsulation LKM hook\n" );
+#endif
 }
