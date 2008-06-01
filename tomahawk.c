9f31f4d98298095ab6e3e1d16b4a16bf391a01a1
dgu <dgu@f494a408-59fc-4d69-83a7-46535463b0e8>
2008-06-01 08:13:22 +0000

    checkpointing now done every iteration => now about 1% deviation (from 10%)
    git-svn-id: svn+ssh://pod.stanford.edu/afs/ir/users/d/g/dgu/svnrepos/mckeown/tomahawk@14 f494a408-59fc-4d69-83a7-46535463b0e8

--- tomahawk.c
+++ tomahawk.c
@@ -35,7 +35,6 @@ Client: -client CLIENT_PARAMETERS\n\
   -bw, -bandwidth: the amount of bandwidth to use in bits per second (including overheads)\n\
   -i, -interval:   interval in milliseconds at which a client will write\n\
   -l, -listen:     the port to listen on for updates to parameters\n\
-  -k, -checkpoint: seconds between rate limiter checkpoints (default=1sec)\n\
 \n\
 Server: -server SERVER_PARAMETERS\n\
   -s, -server:    run tomahawk in server mode\n\
@@ -53,7 +52,6 @@ typedef struct {
     uint16_t  num_flows;
     uint32_t  payload_bytes_per_sec;
     uint32_t  interval_millisec;
-    uint32_t  checkpoint_interval_sec;
     uint16_t  listen_port;
     bool      warned;
 } client_t;
@@ -71,9 +69,9 @@ typedef enum {
     CODE_EXIT
 } code_t;
 
-void* controller_main( void* pclient );
-void client_main( client_t* c );
-void server_main( uint16_t port, uint32_t nap_usec );
+static void* controller_main( void* pclient );
+static void client_main( client_t* c );
+static void server_main( uint16_t port, uint32_t nap_usec );
 
 /**
  * Convert bits to bytes, round up to nearest byte, and then subtract the size
@@ -98,7 +96,6 @@ int main( int argc, char** argv ) {
     /* initialize */
     debug_pthread_init_init();
     memset( &client, 0, sizeof(client) );
-    client.checkpoint_interval_sec = 1;
 
     /* ignore the broken pipe signal */
     signal( SIGPIPE, SIG_IGN );
@@ -180,19 +177,6 @@ int main( int argc, char** argv ) {
                 return -1;
             }
             client.interval_millisec = val;
-        }
-        else if( str_matches(argv[i], 3, "-k", "-checkpoint", "--checkpoint") ) {
-            i += 1;
-            if( i == argc ) {
-                fprintf( stderr, "Error: -checkpoint requires a number to be specified\n" );
-                return -1;
-            }
-            uint32_t val = strtoul( argv[i], NULL, 10 );
-            if( val==0 ) {
-                fprintf( stderr, "Error: %u is not a valid checkpoint interval\n", val );
-                return -1;
-            }
-            client.checkpoint_interval_sec = val;
         }
         else if( str_matches(argv[i], 3, "-l", "-listen", "--listen") ) {
             i += 1;
@@ -276,8 +260,9 @@ int main( int argc, char** argv ) {
 }
 
 static void client_report_stats_and_exit( int code );
+static void client_reset_checkpoint();
 
-void* controller_main( void* pclient ) {
+static void* controller_main( void* pclient ) {
     client_t* c = (client_t*)pclient;
     struct sockaddr_in addr;
     struct sockaddr_in client_addr;
@@ -363,6 +348,7 @@ void* controller_main( void* pclient ) {
                 fprintf( stderr, "controller got unexpected packet code %u\n", packet.code );
             }
             c->warned = FALSE;
+            client_reset_checkpoint();
             debug_println( "avg packet size (on the wire) will be about %uB",
                            OVERHEAD + (c->payload_bytes_per_sec * c->interval_millisec) / (c->num_flows * 1000) );
 
@@ -376,7 +362,7 @@ void* controller_main( void* pclient ) {
     return NULL;
 }
 
-struct timeval time_init;
+static struct timeval time_init;
 static uint64_t total_bytes = 0;
 static uint32_t total_packets = 0;
 static void client_report_stats_and_exit( int code ) {
@@ -406,12 +392,12 @@ Client exiting (%d) ...\n\
     exit( code );
 }
 
-void client_sig_int_handler( int sig ) {
+static void client_sig_int_handler( int sig ) {
     client_report_stats_and_exit( 0 );
 }
 
 /** Same as writen but calls exit if it is unable to write the bytes. */
-inline void writen_or_die( int fd, const void* buf, unsigned n ) {
+static inline void writen_or_die( int fd, const void* buf, unsigned n ) {
     int ret = writen( fd, buf, n );
     if( ret == -1 ) {
         fprintf( stderr, "Error: write of %uB failed for fd=%d: ", n, fd );
@@ -420,19 +406,25 @@ inline void writen_or_die( int fd, const void* buf, unsigned n ) {
     }
 }
 
-void client_main( client_t* c ) {
+static uint64_t checkpointed_prev = 0;
+static uint64_t checkpointed_bytes;
+static struct timeval checkpoint;
+static void client_reset_checkpoint() {
+    gettimeofday( &checkpoint, NULL );
+    checkpointed_bytes = 0;
+}
+
+static void client_main( client_t* c ) {
     struct sockaddr_in servaddr;
-    struct timeval start, end, checkpoint;
-    struct timespec sleep_time;
+    struct timeval start, end;
     uint32_t actual_flows = 0;
     uint32_t req_num_flows;
     bool pause_ok = FALSE;
     uint32_t interval_ms;
     uint32_t Bps, Bps_tot;
     uint32_t num_bytes, extra_bytes = 0;
-    uint32_t checkpointed_bytes = 0, fast_bytes, fast_time, expected_bytes;
+    uint32_t actual_bytes, expected_bytes, time_elapsed;
     int32_t time_ms;
-    uint32_t sleep_ms;
     uint32_t i;
     int fd[MAX_FLOWS];
 
@@ -446,7 +438,7 @@ void client_main( client_t* c ) {
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = c->server_ip;
     servaddr.sin_port = htons( c->server_port );
-    gettimeofday( &checkpoint, NULL );
+    client_reset_checkpoint();
 
     /* one loop per interval */
     while( 1 ) {
@@ -457,7 +449,7 @@ void client_main( client_t* c ) {
         req_num_flows = c->num_flows;
         interval_ms = c->interval_millisec;
         Bps = c->payload_bytes_per_sec;
-        Bps_tot = Bps + OVERHEAD;
+        Bps_tot = Bps + (1000 * OVERHEAD) / interval_ms;
 
         /* create the requested number of flows */
         pause_ok = FALSE;
@@ -519,49 +511,11 @@ void client_main( client_t* c ) {
         total_bytes += ((num_bytes + OVERHEAD) * actual_flows);
         total_packets += actual_flows;
 
-        /* determine how much of the interval is left */
+        /* pause for the rest of the interval */
         gettimeofday( &end, NULL );
         time_ms = get_time_passed_ms( &start, &end );
-
-        /* every so often make sure we aren't sending too fast */
-        if( end.tv_sec - checkpoint.tv_sec > c->checkpoint_interval_sec ) {
-            /* the next checkpoint starts now */
-            checkpoint = end;
-
-            /* compute how many bytes were sent since the last checkpoint */
-            checkpointed_bytes = total_bytes - checkpointed_bytes;
-
-            /* were too many bytes sent? */
-            expected_bytes = c->checkpoint_interval_sec * Bps_tot;
-            if( checkpointed_bytes > expected_bytes ) {
-                /* determine how much extra time we need to spend sleeping */
-                fast_bytes = checkpointed_bytes - expected_bytes;
-                fast_time  = fast_bytes / Bps_tot;
-                time_ms -= (fast_time * 1000);
-
-                debug_println( "checkpoint => fast (over by %uB) (extra sleep of %ums)", fast_bytes, fast_time*1000 );
-
-                /* account for slop thanks to integer division */
-                checkpointed_bytes = total_bytes - (fast_bytes - fast_time * Bps_tot);
-            }
-            else {
-                debug_println( "checkpoint => on time (%uB expected, got %uB)", expected_bytes, checkpointed_bytes );
-                checkpointed_bytes = total_bytes;
-            }
-        }
-
-        /* pause for the remainder of the interval */
-        if( time_ms < (int64_t)interval_ms ) {
-            sleep_ms = interval_ms - time_ms;
-            if( sleep_ms < 1000 )
-                sleep_time.tv_sec = 0;
-            else {
-                sleep_time.tv_sec = sleep_ms / 1000;
-                sleep_ms -= (sleep_time.tv_sec * 1000);
-            }
-            sleep_time.tv_nsec = sleep_ms * 1000 * 1000;
-            nanosleep( &sleep_time, NULL );
-        }
+        if( time_ms < (int64_t)interval_ms )
+            sleep_ms( interval_ms - time_ms );
         else if( !pause_ok ) {
             extra_bytes = (Bps_tot * (time_ms - interval_ms)) / 1000;
             fprintf( stderr, "Warning: interval took too long (took %ums; needed < %ums) (will try to catch up by sending %u extra bytes next interval)\n",
@@ -571,11 +525,25 @@ void client_main( client_t* c ) {
             fprintf( stderr, "Warning: interval took too long (took %ums; needed < %ums) (will not try to catch up because we spent part of the interval creating/destroying flows)\n",
                      time_ms, interval_ms );
         }
+
+        /* what is the total elapsed time */
+        gettimeofday( &end, NULL );
+        time_elapsed = get_time_passed_ms( &checkpoint, &end );
+
+        /* compute how many bytes were sent since the last checkpoint */
+        actual_bytes = total_bytes - checkpointed_bytes - checkpointed_prev;
+
+        /* were too many bytes sent? */
+        expected_bytes = Bps_tot * time_elapsed / 1000;
+        if( actual_bytes > expected_bytes ) {
+            /* determine how much extra time we need to spend sleeping */
+            sleep_ms( (1000 * (actual_bytes - expected_bytes)) / Bps_tot );
+        }
     }
 }
 
 #define JUNK_SIZE 65536
-void server_main( uint16_t port, uint32_t nap_usec ) {
+static void server_main( uint16_t port, uint32_t nap_usec ) {
     struct sockaddr_in addr;
     struct sockaddr_in client_addr;
     unsigned sock_len = sizeof(client_addr);
