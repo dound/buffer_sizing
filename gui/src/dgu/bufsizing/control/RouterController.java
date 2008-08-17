84127146d80b5082c0ccb88c1e57f2e0720307cb
David Underhill <dgu@cs.stanford.edu>
2008-08-17 16:21:25 -0700

    removed get commnads, added support for processing update infos

--- gui/src/dgu/bufsizing/control/RouterController.java
+++ gui/src/dgu/bufsizing/control/RouterController.java
@@ -1,5 +1,7 @@
 package dgu.bufsizing.control;
 
+import dgu.bufsizing.BottleneckLink;
+import dgu.bufsizing.DemoGUI;
 import java.io.IOException;
 
 /**
@@ -8,17 +10,18 @@ import java.io.IOException;
  */
 public class RouterController extends Controller {
     public enum RouterCmd {
-        CMD_GET_RATE((byte)0),    /* kbps */
         CMD_SET_RATE((byte)1),    /* kbps */
-        CMD_GET_BUF_SZ((byte)2),  /* packets */
         CMD_SET_BUF_SZ((byte)3);  /* packets */
         
         public final byte code;
         RouterCmd( byte code ) { this.code = code; }
     }
     
+    UpdateInfoProcessor uip = new UpdateInfoProcessor();
+    
     public RouterController( int port ) {
         super( port );
+        uip.start();
     }
 
     public String getTypeString() {
@@ -54,25 +57,78 @@ public class RouterController extends Controller {
         // use last two bits to specifiy which queue this pertains to
         byte code_plus_queue = (byte)(cmd.code | (queueNum << 6));
         sendCommand( code_plus_queue, value );
-
-        int ret = 0;
-        if( cmd.code == RouterCmd.CMD_GET_BUF_SZ.code || cmd.code == RouterCmd.CMD_GET_RATE.code ) {
-            try {
-                //read each byte in the return value
-                ret =   in.read() << 24;
-                ret += (in.read() << 16);
-                ret += (in.read() << 8);
-                ret +=  in.read();
-                
-                if( cmd.code == RouterCmd.CMD_GET_RATE.code ) {
-                    return translateRateLimitRegToBitsPerSec( ret );
+        return 0;
+    }
+    
+    public class UpdateInfo {
+        public int sec;
+        public int usec;
+        public int arrived;
+        public int departed;
+        public int current; 
+    }
+    
+    /**
+     * Retrieves an update info packet from the stream.
+     */
+    public void receiveUpdate(final UpdateInfo u) throws IOException {
+        u.sec      = readInt();
+        u.usec     = readInt();
+        u.arrived  = readInt();
+        u.departed = readInt();
+        u.current  = readInt();
+    }
+    
+    /**
+     * Process update info packets which are simply summaries of the full event 
+     * capture traffic.
+     */
+    public class UpdateInfoProcessor extends Thread {
+        public static final boolean USE_PACKETS = false;
+        
+        /**
+         * Listens for new event capture packets and processes them with the 
+         * assumption that they are for the first bottleneck on the first router.
+         */
+        public void run() {
+            final UpdateInfo u = new UpdateInfo();
+            long lastErrorMsg_millis = 0;
+            boolean gotUpdate = false;
+            
+            // always assume first bottleneck on the first router for now
+            BottleneckLink b = DemoGUI.me.demo.getRouters().get(0).getBottleneckLinkAt(0);
+            
+            /* listen for updates until the end of time */
+            while (true) {
+                // try to get the next update
+                try {
+                    receiveUpdate(u);
+                    gotUpdate = true;
+                } catch( IOException e ) {
+                    gotUpdate = false;
+                    
+                    if( lastErrorMsg_millis + Controller.TIME_BETWEEN_ERROR_MSGS_MILLIS < System.currentTimeMillis() ) {
+                        System.err.println( "Error: stats receive failed: " + e.getMessage() );
+                        lastErrorMsg_millis = System.currentTimeMillis();
+                    }
+                    
+                    // wait a little before trying again after a failure
+                    DemoGUI.msleep(500);
                 }
-            } catch( IOException e ) {
-                System.err.println( "command " + cmd.code + " / " + value + " => failed: " + e.getMessage() );
-                System.exit( 1 );
+                
+                // process each update we receive
+                if( gotUpdate ) {
+                    long timestamp_usec = ((u.sec * 1000L * 1000L) + u.usec);
+                    long timestamp_8ns = timestamp_usec * 1000L / 8L;
+                    b.setOccupancy( timestamp_8ns, u.current );
+                    b.arrival( timestamp_8ns, u.arrived );
+                    b.departure( timestamp_8ns, u.departed );
+                    
+                    // refresh instantaneous readings over the interval from the previous
+                    //  update to the time of the last event in this update
+                    b.refreshInstantaneousValues( timestamp_8ns );
+                }
             }
         }
-        
-        return ret;
     }
 }
