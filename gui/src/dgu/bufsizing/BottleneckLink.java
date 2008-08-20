eb172e5c05a43b11709e5a8b0de0b209c507ad7f
David Underhill <dgu@cs.stanford.edu>
2008-08-20 01:01:23 -0700

    no longer test with n=1 in auto mode

--- gui/src/dgu/bufsizing/BottleneckLink.java
+++ gui/src/dgu/bufsizing/BottleneckLink.java
@@ -411,7 +411,7 @@ public class BottleneckLink extends Link<Router> {
     }
     
     HashMap<Integer, Result> resultsMea = new HashMap<Integer, Result>();
-    public static final int[] interestingN = new int[]{1,5,10,25,50,75,100,150,200};
+    public static final int[] interestingN = new int[]{5,10,25,50,75,100,150,200};
     
     /** read measured results from measured.txt */
     private void initMeasuredResults() {
